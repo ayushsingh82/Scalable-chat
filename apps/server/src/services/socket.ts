693bdd8ec1d85a27dc23645b6db4b0e77d5d0ab2
Ayush Singh <ayushsinghmi711@gmail.com>
2024-01-06 22:43:24 +0530

    socket provider

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -6,7 +6,25 @@ class SocketService {
 
     constructor(){
         console.log('Init Socket Server..')
-        this._io=new Server()
+        this._io=new Server({
+            cors:{
+                allowedHeaders:["*"],
+                origin:"*",
+            }
+        })
+    }
+
+    public initListeners(){
+        const io=this.io;
+        console.log('Init Socket Listeners...');
+
+        io.on('connect',async socket=>{
+            console.log(`New socket connected`,socket.id);
+
+            socket.on('event:message',async({message}:{message:string})=>{
+                console.log(`New Message rec.`,message)
+            })
+        })
     }
 
     get io(){
