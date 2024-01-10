1a54dfb206c54b621aeda4d12d24c6239ac8523a
Ayush Singh <ayushsinghmi711@gmail.com>
2024-01-10 13:34:31 +0530

    redis aiven connection

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,4 +1,18 @@
 import {Server} from 'socket.io'
+import { Redis } from 'ioredis';
+
+const pub=new Redis({
+    host:'redis-3d25e7c1-ayushsinghmi713-99d8.a.aivencloud.com',
+    port:14649,
+    username:'default',
+    password:'AVNS_qSr5rkx1RAUadx_6HkA'
+});
+const sub= new Redis({
+    host:'redis-3d25e7c1-ayushsinghmi713-99d8.a.aivencloud.com',
+    port:14649,
+    username:'default',
+    password:'AVNS_qSr5rkx1RAUadx_6HkA'
+})
 
 class SocketService {
     private _io:Server;
@@ -12,6 +26,7 @@ class SocketService {
                 origin:"*",
             }
         })
+        sub.subscribe('MESSAGES');
     }
 
     public initListeners(){
@@ -23,8 +38,15 @@ class SocketService {
 
             socket.on('event:message',async({message}:{message:string})=>{
                 console.log(`New Message rec.`,message)
+                //publish this message to redis
+                await pub.publish('MESSAGES',JSON.stringify({message}))
             })
         })
+       sub.on('message',(channel,message)=>{
+        if(channel==='MESSAGES'){
+            io.emit('message',message);
+        }
+       })
     }
 
     get io(){
